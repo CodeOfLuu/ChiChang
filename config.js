bf9781293af6265f2dff7adffcebcd9435141ae5
newwbie97 <fxhoainiem@gmail.com>
2024-07-25 17:43:09 +0700

    Chichang

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Chị Chang à!',
+    introDesc: `Chị nhận ra bức ảnh này chứ`,
+    btnIntro: 'hihi',
+    title: 'Thật ra thì cũng không có gì',
+    desc: 'Tự nhiên hum nay muốn được dẫn đi ăn ',
+    btnYes: 'Chị dẫn em đi <33',
+    btnNo: 'Hông, bé ơi :3',
+    question:'Xác nhận lại lần nữa nè <3',
+    btnReply: 'Gửi cho Lưu <3',
+    reply: 'Oke lun <33333333',
+    mess: 'Tuyệt vời 🥰. Xương Chị nhiều nhiều 😘😘',
+    messDesc: 'Thời gian tùy ý Chị nhé.',
+    btnAccept: 'Chốt',
+    
+    
+}
